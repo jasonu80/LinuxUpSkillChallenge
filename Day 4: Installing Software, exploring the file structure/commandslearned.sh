d69034f87b7fd59429038cdbe3e2853393bf83c8
jasonu80 <79412658+jasonu80@users.noreply.github.com>
2025-11-19 20:13:38 +1100

    Add files via upload

---
+++ Day 4: Installing Software, exploring the file structure/commandslearned.sh
@@ -0,0 +1,14 @@
+#search for package name
+apt search "<name>"
+
+#install the package
+apt install "<pkg name>"
+
+#Midnight Commander -> TUI File Manager
+mc
+
+#install hangman please...
+<answer here>
+
+#advanced:
+	#use mc to view /etc/apt/sources.list.d/ubuntu.sources

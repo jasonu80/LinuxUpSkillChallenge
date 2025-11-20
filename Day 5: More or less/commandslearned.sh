94ad019ba4c74c810a7613ec1b71448766d7b91d
jasonu80 <79412658+jasonu80@users.noreply.github.com>
2025-11-20 15:54:09 +1100

    Add files via upload

---
+++ Day 5: More or less/commandslearned.sh
@@ -0,0 +1,17 @@
+#using more and less for viewing files
+more/less <file>
+
+#history command -- commands used since first time logged in
+history
+
+#check commands used in history
+!number
+
+#hit ctrl + r for typing a part of the command that you want to look for -- for completing certain commands
+
+#Look at hidden files in a home directory
+ls -ltra ~
+less ~/.bashrc
+
+#Other shell sessions: screen, tmux
+
